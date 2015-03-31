6b0d1d339a840389d8dc76eb57b0072383af61ab
A. Sean Pue <pue@msu.edu>
2015-03-30 20:27:21 -0400

    finished part 1 of tutorial

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):              # __unicode__ on Python 2
         return self.question_text
 
