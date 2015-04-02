1d9e3a0f2942634d36a10058becddf88e2148c34
A. Sean Pue <pue@msu.edu>
2015-04-02 19:52:47 -0400

    added tests - tutorial part 5

--- django1/urls.py
+++ django1/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+        url(r'^polls/', include('polls.urls', namespace="polls")),
+        #url(r'^polls/', include('polls.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+    )
+    
